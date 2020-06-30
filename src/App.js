19f459e6faa4d1881c50a6b5ea0fcf448b408cba
Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-06-29 23:10:40 -0400

    Movie App in the making

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import randomcolor from 'randomcolor'
+
 
 function App() {
+  const [color, setColor] = useState(randomcolor)
+
+  const colorChange = () => {
+    setColor(randomcolor)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{color: color}}> Movie App In The Making</h1>
+      <button onClick={colorChange}>Click Me!</button>
     </div>
   );
 }
